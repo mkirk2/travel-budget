fa891c620d75ff0a6a23057b4c1b85587b9678a5
Matt Kirk <matt.22222@hotmail.com>
2018-05-04 13:36:08 -0700

    welcome complete

--- app/assets/javascripts/welcome_form.js
+++ app/assets/javascripts/welcome_form.js
@@ -1,5 +1,22 @@
 $( document ).ready(function() {
-    $( "#welcome-submit" ).click(function() {
-    console.log( "ready!" );
+    $( "#welcome-submit" ).click(function(event) {
+        event.preventDefault();
+        $( "#welcome-output" ).show();
+        var input1 = parseInt($("#budget-total").val());
+        var input2 = parseInt($("#length-total").val());
+        var output = Math.round(input1/input2);
+        var nameInput = capitalize($("#welcome-name").val());
+        if (($("#budget-total").val()).length > 0 && $("#length-total").val().length > 0) {
+            $( "#welcome-output" ).text("The budget for your trip is " + output + " USD per day. Have a great trip " + nameInput + ".");
+        } else {
+            $( "#welcome-output" ).text("Please input your budget and trip length.");
+        }
+    });
+    $( "#welcome-reset" ).click(function(){
+        $( "#welcome-output" ).hide();
     });
 });
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
